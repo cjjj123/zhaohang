faf1ea187333272bd01c305dc319164f1170963b
cjjj123 <465313669@qq.com>
2020-10-12 17:55:02 +0800

    test

---
+++ src/main/java/com/cjj/zhaohang/ZhaohangApplication.java
@@ -0,0 +1,13 @@
+package com.cjj.zhaohang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhaohangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhaohangApplication.class, args);
+    }
+
+}
